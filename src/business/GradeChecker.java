df2699b17653913557fe3ab6f3050c2083fbf84f
gintamaofgin <52989494+gintamaofgin@users.noreply.github.com>
2019-07-17 19:21:51 +0700

    Update GradeChecker.java

--- src/business/GradeChecker.java
+++ src/business/GradeChecker.java
@@ -12,7 +12,7 @@ package business;
 public class GradeChecker {
     public static char checkGrade(double grade){
         if(grade > 10 || grade < 0){
-            return 'o';
+            return 'e';
         }else{
             if(grade >= 5.0){
             return 'p';
